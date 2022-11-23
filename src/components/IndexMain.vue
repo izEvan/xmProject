9361a2931dcf645fd57fa5db2fa8cd5310df3d77
izEvan <3068198552@qq.com>
2022-11-24 02:20:04 +0800

    初步完成商品模块

---
+++ src/components/IndexMain.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="index-main">
+    <div class="news">
+      <router-link to="/">
+        <img src="/image/index/news.jpg" alt="">
+      </router-link>
+    </div>
+    <div class="phone">
+      <div class="phone_nav">
+        <h2>手机</h2>
+          <router-link to="/login">
+            查看更多
+            <span></span>
+          </router-link>
+      </div>
+      <div class="phone_content">
+        <div class="phone_content_left">
+          <router-link to="/">
+            <img src="/image/index/product.jpg" alt="">
+          </router-link>
+        </div>
+        <div class="phone_content_right">
+          <ul>
+            <li v-for="i in 8" :key="i">
+              <router-link to="/login" class="nav_content">
+                <div>
+                  <img src="/image/index/pic.jpg" alt="">
+                </div>
+                <h3>Redmi Note 12 5G</h3>
+                <p>三星 OLED 护眼屏｜骁龙 5G 芯｜5000mAh 电量</p>
+                <p>1199 元<del>1199 元</del></p>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="phone">
+      <div class="phone_nav">
+        <h2>手机</h2>
+          <router-link to="/login">
+            查看更多
+            <span></span>
+          </router-link>
+      </div>
+      <div class="phone_content">
+        <div class="phone_content_left">
+          <router-link to="/">
+            <img src="/image/index/product.jpg" alt="">
+          </router-link>
+        </div>
+        <div class="phone_content_right">
+          <ul>
+            <li v-for="i in 8" :key="i">
+              <router-link to="/login" class="nav_content">
+                <div>
+                  <img src="/image/index/pic.jpg" alt="">
+                </div>
+                <h3>Redmi Note 12 5G</h3>
+                <p>三星 OLED 护眼屏｜骁龙 5G 芯｜5000mAh 电量</p>
+                <p>1199 元<del>1199 元</del></p>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style src='../assets/css/main.css' scoped></style>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,14 +2,16 @@
   <div class="index">
     <public-header />
     <index-carousel />
+    <index-main />
   </div>
 </template>
 
 <script>
 import PublicHeader from '@/components/PublicHeader.vue'
 import IndexCarousel from '@/components/IndexCarousel.vue'
+import IndexMain from '@/components/IndexMain.vue'
   export default {
-  components: { PublicHeader, IndexCarousel },
+  components: { PublicHeader, IndexCarousel, IndexMain },
   data() {
     return {
         
